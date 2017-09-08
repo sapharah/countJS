97b7a1fa48874f5020c6a98c42235b616c6add6b
sapharah <sapharah.prescod@pepperdine.edu>
2017-09-07 21:54:48 -0500

    parse webpage

--- count.js
+++ count.js
@@ -1,2 +1,11 @@
-<script type="text/javascript">
-<script>
+(function analyzeText(){
+    common = [];
+    exceptions = ["is", "are", "was", "were"];
+    var text = $("body").text();
+    var strippedStr = bodyText.replace(/[^\w\s]/gi, "").replace(/\(.*;?\;/g, "").replace(/\w{20}/ig, "").replace(/[0-9]/g, "").replace(/\s\s+/g, ' ').replace(/(\b(\w{1,1})\b(\W|$))/g, "");
+    
+    //obtain top 100 words
+    $.get("https://en.wikipedia.org/wiki/Most_common_words_in_English", function(data){
+        
+    })
+});
